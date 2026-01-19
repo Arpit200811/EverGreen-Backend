40b5d58712a4ff1fcde025841a62798554e89a9e
Arpit200811 <arpitgupta9610@gmail.com>
2026-01-19 12:35:53 +0530

    19-01-2026

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ const io = initIo(server);
 require('./cron/attendance.cron');
 
 app.use(cors({
-  origin: "http://localhost:5173", //https://evergreen-frontend.onrender.com   http://localhost:5173
+  origin: "https://evergreen-frontend.onrender.com", //https://evergreen-frontend.onrender.com   http://localhost:5173
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
